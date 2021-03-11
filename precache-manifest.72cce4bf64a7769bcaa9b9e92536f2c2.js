a5824501ec8698c485168f5e0352a60c454f7fae
ciamac-da <ciamac.davoudi@gmail.com>
2021-03-11 13:25:12 +0100

    Updates

---
+++ precache-manifest.72cce4bf64a7769bcaa9b9e92536f2c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f59f9ed08d4bf341db83cfa7a3e869d",
+    "url": "/react-material-ui-search-filter/index.html"
+  },
+  {
+    "revision": "cfb81b9a72721115f4e8",
+    "url": "/react-material-ui-search-filter/static/css/main.b3e5151c.chunk.css"
+  },
+  {
+    "revision": "74e15cb0c04f9e0992a0",
+    "url": "/react-material-ui-search-filter/static/js/2.4dc737cb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-material-ui-search-filter/static/js/2.4dc737cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb81b9a72721115f4e8",
+    "url": "/react-material-ui-search-filter/static/js/main.abf238e2.chunk.js"
+  },
+  {
+    "revision": "ee8a3e6f1d34092a1978",
+    "url": "/react-material-ui-search-filter/static/js/runtime-main.9fce7d77.js"
+  }
+]);
